379418fc51e911317432bcc1e8cedf8dbd95d94d
SpectralPixel <encrypteddvjjrxv@gmail.com>
2025-04-17 17:03:31 +0200

    Add Ray constructor

--- src/ray.rs
+++ src/ray.rs
@@ -6,3 +6,9 @@ pub struct Ray {
     pub position: Vec2,
     pub direction: Vec2,
 }
+
+impl Ray {
+    pub fn new(position: Vec2, direction: Vec2) -> Self {
+        Self { position, direction }
+    }
+}

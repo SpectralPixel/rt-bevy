73464cac73e634a655c4eb01f284cdf72495c761
SpectralPixel <encrypteddvjjrxv@gmail.com>
2025-04-23 01:16:37 +0200

    Add more fieilds to `RayViewport2D`

--- src/viewport.rs
+++ src/viewport.rs
@@ -6,18 +6,29 @@ pub struct RayViewport2D {
     direction: Direction, // implement Into<Vec2> for Direction
     forward: Vec2,
     fov: f32,
-    ray_count: u16
+    near_plane: f32, // near clipping plane distance
+    ray_count: u16,
+    left_edge_position: Vec2,
+    right_edge_position: Vec2,
 }
 
 impl RayViewport2D {
-    pub fn new(origin: Vec2, direction: Direction, fov_degrees: f32, ray_count: u16) -> Self {
-        let forward = Vec2::from_angle(direction.get());
+    pub fn new(origin: Vec2, direction: Direction, fov_degrees: f32, near_plane: f32, ray_count: u16) -> Self {
+        let forward = Vec2::from_angle(direction.get()).normalize();
+        let fov = fov_degrees.to_radians();
+        let left = forward.perp();
+        let right = -left;
+        let left_edge_position = origin + forward * near_plane + left * (fov / 2.) * near_plane;
+        let right_edge_position = origin + forward * near_plane + right * (fov / 2.) * near_plane;
         Self {
             origin,
             direction,
             forward,
             fov: fov_degrees.to_radians(),
-            ray_count
+            near_plane,
+            ray_count,
+            left_edge_position,
+            right_edge_position,
         }
     }
 }

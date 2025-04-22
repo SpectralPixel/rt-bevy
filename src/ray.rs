eb95d7cef9088b256cabfad5989dbc5e356288d7
SpectralPixel <encrypteddvjjrxv@gmail.com>
2025-04-23 00:55:20 +0200

    Rays can draw their own gizmos now

--- src/ray.rs
+++ src/ray.rs
@@ -1,7 +1,5 @@
 use bevy::prelude::*;
 
-const GRID_SIZE: u8 = 10;
-
 pub struct Ray {
     pub position: Vec2,
     pub direction: Vec2,
@@ -9,6 +7,17 @@ pub struct Ray {
 
 impl Ray {
     pub fn new(position: Vec2, direction: Vec2) -> Self {
-        Self { position, direction }
+        Self {
+            position,
+            direction,
+        }
+    }
+
+    pub fn draw_gizmo(&self, mut gizmos: Gizmos, length: f32) {
+        gizmos.arrow_2d(
+            self.position,
+            self.position + self.direction * length,
+            Color::linear_rgb(1., 0., 0.),
+        );
     }
 }

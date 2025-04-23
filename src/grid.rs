15c0aa0a636b67caf5839ff403e8965149dea294
SpectralPixel <encrypteddvjjrxv@gmail.com>
2025-04-23 16:08:14 +0200

    Add `GridPlugin` and initialize `Grid2D` at game start

--- src/grid.rs
+++ src/grid.rs
@@ -1,7 +1,23 @@
+use bevy::prelude::*;
+
+pub struct GridPlugin;
+
+impl Plugin for GridPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, initialize);
+        //app.add_systems(Update, viewport_gizmo);
+    }
+}
+
+#[derive(Component)]
 pub struct Grid2D {
     width: f32,
     height: f32,
     cell_size: f32,
+}
+
+fn initialize(mut commands: Commands) {
+    commands.spawn(Grid2D::new(15., 15., 50.));
 }
 
 impl Grid2D {
